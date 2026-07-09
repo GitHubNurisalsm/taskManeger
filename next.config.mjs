fa1cfccd28e90910b3d45a00158f65b80d1d57a2
GitHubNurislam <vipbaron06@gmail.com>
2026-07-09 18:46:46 +0600

    Fix next config duplicate declaration

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
-
-export default nextConfig
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   output: "export",
   basePath: "/taskManager",
   assetPrefix: "/taskManager/",
