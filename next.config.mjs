88ce7690a61e577aec00f6a1fe7f01f929308752
GitHubNurislam <vipbaron06@gmail.com>
2026-07-09 19:25:59 +0600

    Fix next next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
-  basePath: "/taskManager",
-  assetPrefix: "/taskManager/",
+  trailingSlash: true,
+
+  basePath: isProd ? "/taskManager" : "",
+  assetPrefix: isProd ? "/taskManager/" : "",
+
   images: {
     unoptimized: true,
   },
